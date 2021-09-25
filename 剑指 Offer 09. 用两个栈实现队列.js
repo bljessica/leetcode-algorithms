8c4b043551a6096b492d7e8821aa05693e187d76
bljessica <1194620082@qq.com>
2021-09-25 20:33:08 +0800

    2021/9/25

---
+++ 剑指 Offer 09. 用两个栈实现队列.js
@@ -0,0 +1,34 @@
+var CQueue = function() {
+  this.stack1 = []
+  this.stack2 = []
+};
+
+/** 
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function(value) {
+  this.stack2.push(value)
+};
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function() {
+  if (!this.stack1.length && !this.stack2.length) {
+    return -1
+  }
+  if (!this.stack1.length) {
+    while (this.stack2.length) {
+      this.stack1.push(this.stack2.pop())
+    }
+  } 
+  return this.stack1.pop()
+};
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * var obj = new CQueue()
+ * obj.appendTail(value)
+ * var param_2 = obj.deleteHead()
+ */
