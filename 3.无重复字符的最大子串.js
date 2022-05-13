54a74e163b094c0ddfbfe204b0e5d35335cc5df6
bljessica <1194620082@qq.com>
2022-05-13 13:26:54 +0800

    fix: modify 3

--- 3.无重复字符的最大子串.js
+++ 3.无重复字符的最大子串.js
@@ -2,19 +2,35 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    let maxLen = 0, str = '', start = 0;
-    for(let i = 0; i < s.length; i++) {
-        if(str.indexOf(s[i]) === -1) {
-            str += s[i];
-            maxLen = Math.max(maxLen, str.length);
-        }
-        else {
-            let index = str.indexOf(s[i]);
-            start = index + 1;
-            str = str.substring(start);
-            str += s[i];
-        }
+ var lengthOfLongestSubstring = function(s) {
+  // 1.
+  // if (s.length === 0) return 0
+  // let max = 1
+  // for (let i = 0; i < s.length; i++) {
+  //   const arr = [s[i]]
+  //   for (let j = i + 1; j < s.length; j++) {
+  //     if (arr.includes(s[j])) {
+  //       break
+  //     } else {
+  //       max = Math.max(max, j - i + 1)
+  //       arr.push(s[j])
+  //     }
+  //   }
+  // }
+  // return max
+  // 2.滑动窗口
+  const set = new Set()
+  const len = s.length
+  let max = 0, right = -1
+  for (let i = 0; i < len; i++) {
+    if (i !== 0) { // 左指针右移
+      set.delete(s[i - 1])
     }
-    return maxLen;
+    while (right + 1 < len && !set.has(s[right + 1])) {
+      set.add(s[right + 1])
+      right++
+    }
+    max = Math.max(max, right - i + 1)
+  }
+  return max
 };
